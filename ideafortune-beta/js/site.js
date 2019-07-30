049c54418d8d95b10c90ff69894290a2a773f0e5
Sudirman Sudirman <nukecpower@gmail.com>
2019-07-30 17:19:57 +0700

    Updates

--- ideafortune-beta/js/site.js
+++ ideafortune-beta/js/site.js
@@ -17,6 +17,28 @@ $(document).ready(function () {
     ]
   });
 
+  var featuresCarousel = $(".system-features-slides").owlCarousel({
+    loop: true,
+    margin: 0,
+    dots: false,
+    nav: false,
+    mouseDrag: false,
+    items: 1,
+    lazyLoad: true,
+    center: true,
+    autoplay: true,
+    autoplayTimeout: 4000,
+    autoplayHoverPause: true
+  });
+
+  $('.next-feature').click(function () {
+    featuresCarousel.trigger('next.owl.carousel');
+  });
+  $('.prev-feature').click(function () {
+    featuresCarousel.trigger('prev.owl.carousel');
+  });
+
+
   $("#arrow").click(function () {
     $.fn.pagepiling.moveSectionDown();
   });
